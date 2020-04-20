edbe736501638073f150d3e4ed80a5157ec26c5a
durrjp <durrjp1@gmail.com>
2020-04-20 09:40:35 -0500

    initial commit

---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels West</h3>
+        <div className="location__address">123 Main Street</div>
+    </section>
+)
